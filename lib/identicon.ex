4022ff98c32ee12a8d5ae4101d294fb4d25e98e9
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-11-11 18:54:58 -0500

    pattern matching to pick color from first 3 elements in the hex array

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,16 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  # pattern matching inside the argument to the function
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    # will put hex list in hex_list from image struct
+    # _tail is the rest of the elements. We put _ as we are not using it.
+
+    %Identicon.Image{image | color: { r, g, b}}
+    # create a new record with the existing properties and adding the color
   end
 
   def hash_input(input) do
